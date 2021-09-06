6d1d15ffa863ce0dbafa561f5969b6616d4568eb
Nguyễn Thị Thúy <thuynguyen969@gmail.com>
2021-09-06 13:16:47 +0700

    baitap

--- homework3.py
+++ homework3.py
@@ -1,22 +1,35 @@
 # Bài 5: Print Star
 # Hãy viết chương trình in ra các hình sau (dùng ký tự '*' và ký tự space) với n là số dòng. Vd: n = 4:
-print("Bài 5: chương trình in ra các hình sau (dùng ký tự '*' và ký tự space) với n là số dòng")
-n = int(input("Nhập số dòng:"))
+# Bài 5 - Hình 1:
+print("Bài 5 - Hình 1: chương trình in ra các hình sau (dùng ký tự '*' và ký tự space) với n là số dòng")
+n = int(input("Nhập số sao lớn nhất muốn in ra: "))
 i = 1
 j = n-1
 sao = '*'
 space = ' '
-print("Hình 1")
-while (i <= n):
-    print(space*j, (space+sao)*i, i, j)
+for i in range(1,n+1):
+    print(space*j, sao*i)
     i += 1
     j -= 1
 
-# while (i<=n):
-#        print(sao*j,space*i)
-#        i += 1;
-#        j -=1;
-
+# Bài 5 - Hình 2:
+print("Bài 5 - Hình 2: chương trình in ra các hình sau (dùng ký tự '*' và ký tự space) với n là số dòng")
+n = int(input("Nhập số sao lớn nhất muốn in ra: "))
+i = 1
+j = n-1
+sao = '*'
+space = ' '
+for i in range(1,n+1):
+  if(i==n):
+    print(j*space,n*2*sao)      
+  else:
+    print(j*space,i*sao) 
+  i+=1
+  j-=1
+for j in range(1,n+1):
+  i-=1
+  if(i!=n):
+    print(n*space,i*sao) 
 
 # Bài 4: Viết chương trình trả ra từ điển với key là các số trong list, value là số lần xuất hiện của số trong list
 """
@@ -44,18 +57,21 @@ Countdown to Xmas 2021: 112 days, 11:47:01.339588
 Countdown to Xmas 2021: 112 days, 11:46:56.324008
 Countdown to Xmas 2021: 112 days, 11:46:51.310473
 """
+###Tổ trưởng/ Giáo viên nếu chạy cả file thì bỏ comment phần số lần cần hiển thị n, i để dừng sớm như mog muốn nhé
+
 print("Bài 3: Đếm ngược đến XMas 2021")
 import time
 import datetime
 sleep = int(input("\tNhập thời gian cần đợi (s): "));
-n = int(input("\tSố lần cần hiển thị: "));
-i = 1;
+#n = int(input("\tSố lần cần hiển thị: "));
+#i = 1;
 now = datetime.datetime.now()
 xmas = datetime.datetime(2021, 12, 25, 0, 0, 0)
 t = xmas - now;
-while (t and i <= n):
+#while (t and i <= n):
+while t:
     now = datetime.datetime.now()
-    i+=1;
+    #i+=1;
     print('\tCountdown to Xmas 2021: ' +str(xmas - now));
     time.sleep(sleep)
 
@@ -64,7 +80,21 @@ while (t and i <= n):
 """Cho một list gồm 1 hoặc nhiều từ điển (Dictionary). Viết chương trình để trả ra tập hợp tất cả các giá trị (values) duy nhất trong tất cả danh sách các từ điển trên.
 #[VD1]: Trường hợp dưới đây danh sách đầu vào có 1 từ điển map tên người vào tuổi của họ. Trả ra set các tuổi duy nhất."""
 print("Bài 2: Unique value Dictionary")
+def unique_value_dict(mydict):
+    list=[]
+    set1=set()
+    for i in mydict:
+        value = i.values()
+        for j in value:
+            list.append(j)
+    set1=set(list)
+    print("\tGiá trị duy nhất của value",set1)
 
+list_1= [{"V":"S001"},{"V": "S002"},{"VI": "S001"},{"VI": "S005"},{"VII":"S005"},{"V":"S009"},{"VIII":"S007"}]
+list_2=[dict(Trang=38, Thu=38, Ngoc=27, Thanh=26, Yen=25, Hang=22, Thuy=22)]
+
+unique_value_dict(list_1)
+unique_value_dict(list_2)
 
 # Bài 1: Find Pair
 # cho list A chứa các số nguyên đã sắp xếp theo thứ tự tăng dần.
